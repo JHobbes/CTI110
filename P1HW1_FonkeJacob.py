2ec1bf65df5d6f15a356b33447d4cd67748fc7a8
JHobbes <160517616+JHobbes@users.noreply.github.com>
2024-02-19 11:26:29 -0500

    Add files via upload
    First submission of P1HW1

---
+++ P1HW1_FonkeJacob.py
@@ -0,0 +1,26 @@
+#Jacob Fonke
+#2/19/24
+#P1HW1-Basic output with variables
+
+
+first_int = int(input("Enter integer:"))
+print("You entered:", first_int)
+
+squared_int = first_int ** 2
+
+print( first_int,"squared is", squared_int)
+
+cubed_int = first_int ** 3
+
+print("And", first_int,"cubed is", cubed_int,"!!")
+
+second_int = int(input("Enter another integer:"))
+
+added_int = first_int + second_int
+
+multiply_int = first_int * second_int
+
+print( first_int,"+", second_int, "is", added_int)
+print( first_int,"*", second_int, "is", multiply_int)
+
+
